6cd119b1128b797ef93d3051c63d2f9d98ce00e9
BhabhaSeptember <septemberbhabha@gmail.com>
2025-07-10 13:51:14 +0200

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "./",
   build: {
-    outDir: 'dist',  // Explicitly set the output directory to 'dist'
+    outDir: "dist", // Explicitly set the output directory to 'dist'
   },
   server: {
     port: 3000,
-    open: true
+    open: true,
   },
 });
